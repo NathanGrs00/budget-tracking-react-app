81a1e656e1648d21d53f0fb43b4d86482da5ff33
NathanGRSDesktop <nathangeers00@gmail.com>
2025-02-19 18:58:53 +0100

    Feat: Reset Button resets useStates

--- src/components/NewIncome.tsx
+++ src/components/NewIncome.tsx
@@ -1,14 +1,17 @@
 import Form from 'react-bootstrap/Form';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Button, Stack} from "react-bootstrap";
+import { useState } from "react";
 
 function NewIncome() {
+    const [incomeName, setIncomeName] = useState("");
+    const [incomeAmount, setIncomeAmount] = useState("");
     return (
         <Stack direction="horizontal" gap={3}>
-            <Form.Control className="me-auto" placeholder="Name of income e.g. 'Salary'" />
-            <Form.Control className="me-auto" placeholder="Amount" />
+            <Form.Control className="me-auto" placeholder="Name of income e.g. 'Salary'" value={incomeName} onChange={(e) =>setIncomeName(e.target.value)}/>
+            <Form.Control className="me-auto" placeholder="Amount" value={incomeAmount} onChange={(e) => setIncomeAmount(e.target.value)} />
             <div className="vr" />
-            <Button variant="outline-danger">Reset</Button>
+            <Button variant="outline-danger" onClick={function(){setIncomeName(""); setIncomeAmount("")}}>Reset</Button>
         </Stack>
     );
 }
