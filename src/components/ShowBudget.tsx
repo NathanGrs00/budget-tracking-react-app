eb4be8105c56aecccc76edd47049728b012233d6
NathanGRSDesktop <nathangeers00@gmail.com>
2025-02-20 23:33:03 +0100

    Refactor: Don't show pie chart if no income is filled in.

--- src/components/ShowBudget.tsx
+++ src/components/ShowBudget.tsx
@@ -13,7 +13,11 @@ function ShowBudget({allExpenses, totalIncome, totalExpenses}: Props) {
     return (
         <div className={"container shadow p-5 mt-5 rounded-4"}>
             <h2 className={"mb-5"}>Monthly expenses:</h2>
-            <BasicPie allExpenses={allExpenses} totalIncome={totalIncome} totalExpenses={totalExpenses}/>
+            {totalIncome === 0 ? (
+                <h4>No income selected yet</h4>
+            ) : (
+                <BasicPie allExpenses={allExpenses} totalIncome={totalIncome} totalExpenses={totalExpenses}/>
+            )}
         </div>
     )
 }
